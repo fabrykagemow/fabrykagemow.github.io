76222e2e8980b1c8bb17251987e3bbffff246157
fabrykagemow <55050860+fabrykagemow@users.noreply.github.com>
2019-09-11 18:34:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ jQuery("#button-dalej").click(function() {
          function()
          { 
          alert("Klikając DALEJ oświadczasz, że akceptujesz regulamin strony.");
-         }, 401);
+         }, 510);
 
 
       }, 4500);
